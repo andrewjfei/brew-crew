8b368b34fefcc1ad3082ee8974cad21adf77a6d8
andrewjfei <andrewjfei@gmail.com>
2020-03-22 18:30:15 +1300

    added services folder, and app working on android only (not set up for iOS)

---
+++ lib/services/auth.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+// Underscore means the property is private
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign in anon
+
+  // Sign in with email and passsword
+
+  // Register with email and password
+
+  // Sign out
+
+}
